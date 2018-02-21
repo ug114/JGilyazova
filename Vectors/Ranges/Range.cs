9331a3af14ffb1da2c8de34e012bef337b7319c4
user <user@WIN-2KR6UHLB0NV>
2018-02-21 12:36:48 +0700

    Добавили Range.

---
+++ Vectors/Ranges/Range.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranges
+{
+    class Range
+    {
+        private double to;
+        private double from;
+
+        public Range(double from, double to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public double To
+        {
+            get
+            {
+                return to;
+            }
+            set
+            {
+                to = value;
+            }
+        }
+
+        public double From
+        {
+            get
+            {
+                return from;
+            }
+            set
+            {
+                from = value;
+            }
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.to - this.from;
+            }
+        }
+
+        public bool IsInside(double number)
+        {
+            return number <= this.to && number >= this.from;
+        }
+
+        public static Range GetIntersection(Range range1, Range range2)
+        {
+            if (Math.Max(range1.From, range2.From) < Math.Min(range1.To, range2.To))
+            {
+                return new Range(Math.Max(range1.From, range2.From), Math.Min(range1.To, range2.To));
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static Range[] GetUnion(Range range1, Range range2)
+        {
+            if (Math.Max(range1.From, range2.From) > Math.Min(range1.To, range2.To))
+            {
+                return new Range[] { new Range(range1.From, range1.To), new Range(range2.From, range2.To) };
+            }
+            else
+            {
+                return new Range[] { new Range(Math.Min(range1.From, range2.From), Math.Max(range1.To, range2.To)) };
+            }
+        }
+
+        public static Range[] GetDifference(Range range1, Range range2)
+        {
+            if (range1.From == range2.From && range1.To == range2.To)
+            {
+                return null;
+            }
+            if (range2.From > range1.From && range2.To < range1.To)
+            {
+                return new Range[] { new Range(range1.From, range2.From), new Range(range2.To, range1.To) };
+            }
+            if (range2.To < range1.To)
+            {
+                return new Range[] { new Range(range2.To, range1.To) };
+            }
+            if (range2.From < range1.To)
+            {
+                return new Range[] { new Range(range1.From, range2.From) };
+            }
+
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            Range range1 = new Range(1, 2);
+            Range range2 = new Range(1, 1.5);
+            Console.WriteLine("Длина интервала равна {0}.", range1.Length);
+            double number = 10.0;
+
+            Range rangeResult = Range.GetIntersection(range1, range2);
+            
+            Range[] arrayResult = Range.GetDifference(range1, range2);
+
+            if (arrayResult != null)
+            {
+                foreach (Range range in arrayResult)
+                {
+                    Console.WriteLine(range.From + "," + range.To);
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+            if (range1.IsInside(number))
+            {
+                Console.WriteLine("Число {0} внутри диапазона от {1} до {2}.", number, range1.From, range1.To);
+            }
+            else
+            {
+                Console.WriteLine("Число {0} не принадлежит диапазону от {1} до {2}.", number, range1.From, range1.To);
+            }
+        }
+    }
+}
--- Vectors/Vectors/Vectors.cs
+++ Vectors/Vectors/Vectors.cs
@@ -6,161 +6,152 @@ using System.Threading.Tasks;
 
 namespace Vectors
 {
-    //class Vectors
-    //{
-        class Vector
-        {
-            private double[] components;
+    class Vector
+    {
+        private double[] components;
 
-            public Vector(int n)
+        public Vector(int n)
+        {
+            //try
+            //{
+            //    if (n <= 0)
+            //    {
+            //        throw new IllegalArgumentException();
+            //    }
+            //}
+            //catch (IllegalArgumentException)
+            //{
+            //    Console.WriteLine("Exception");
+            //}
+            this.components = new double[n];
+            for (int i = 0; i < n; i++)
             {
-                //try
-                //{
-                //    if (n <= 0)
-                //    {
-                //        throw new IllegalArgumentException();
-                //    }
-                //}
-                //catch (IllegalArgumentException)
-                //{
-                //    Console.WriteLine("Exception");
-                //}
-                this.components = new double[n];
+                this.components[i] = 0;
+            }
+        }
+
+        public Vector(Vector vector)
+        {
+            for (int i = 0; i < vector.getSize(); i++)
+            {
+                this.components[i] = vector.components[i];
+            }
+        }
+
+        public Vector(double[] array)
+        {
+            this.components = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                this.components[i] = array[i];
+            }
+        }
+
+        public Vector(int n, double[] array)
+        {
+            this.components = new double[n];
+            if (array.Length == n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    this.components[i] = array[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    this.components[i] = array[i];
+                }
+
                 for (int i = 0; i < n; i++)
                 {
                     this.components[i] = 0;
                 }
             }
+        }
 
-            public Vector(Vector vector)
+        public int getSize()
+        {
+            return this.components.Length;
+        }
+
+        public string toString()
+        {
+            string outputString = this.components[0].ToString();
+
+            for (int i = 1; i < this.getSize(); i++)
             {
-                for (int i = 0; i < vector.getSize(); i++)
-                {
-                    this.components[i] = vector.components[i];
-                }
+                outputString = outputString + " ," + this.components[i];
             }
+            return outputString;
+        }
 
-            public Vector(double[] array)
+        public Vector GetSum(Vector vector2)
+        {
+            for (int i = 0; i < this.getSize(); i++)
             {
-                this.components = new double[array.Length];
-                for (int i = 0; i < array.Length; i++)
-                {
-                    this.components[i] = array[i];
-                }
+                this.components[i] += vector2.components[i];
             }
+            return this;
+        }
 
-            public Vector(int n, double[] array)
+        public Vector GetDifference(Vector vector2)
+        {
+            for (int i = 0; i < this.getSize(); i++)
             {
-                this.components = new double[n];
-                if (array.Length == n)
-                {
-                    for (int i = 0; i < n; i++)
-                    {
-                        this.components[i] = array[i];
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < array.Length; i++)
-                    {
-                        this.components[i] = array[i];
-                    }
-
-                    for (int i = 0; i < n; i++)
-                    {
-                        this.components[i] = 0;
-                    }
-                }
+                this.components[i] -= vector2.components[i];
             }
+            return this;
+        }
 
-            public int getSize()
+        public Vector GetMultiplication(double scalar)
+        {
+            for (int i = 0; i < this.getSize(); i++)
             {
-                return this.components.Length;
+                this.components[i] *= scalar;
             }
+            return this;
+        }
 
-            public string toString()
+        public Vector Reverse()
+        {
+            for (int i = 0; i < this.getSize(); i++)
             {
-                string outputString = this.components[0].ToString();
-
-                for (int i = 1; i < this.getSize(); i++)
-                {
-                    outputString = outputString + " ," + this.components[i];
-                }
-                return outputString;
+                this.components[i] *= -1;
             }
+            return this;
+        }
 
-            public Vector GetSum(Vector vector2)
+        public double GetComponent(int index)
+        {
+            return this.components[index];
+        }
+
+        public void SetComponent(int index, double newComponent)
+        {
+            this.components[index] = newComponent;
+        }
+
+        public static Vector GetSum(Vector vector1, Vector vector2)
+        {
+            return vector1.GetSum(vector2);
+        }
+
+        public static Vector GetDifference(Vector vector1, Vector vector2)
+        {
+            return vector1.GetDifference(vector2);
+        }
+
+        public static double GetMultiple(Vector vector1, Vector vector2)
+        {
+            double sum = 0;
+            for (int i = 0; i < vector1.getSize(); i++)
             {
-                for (int i = 0; i < this.getSize(); i++)
-                {
-                    this.components[i] += vector2.components[i];
-                }
-                return this;
+                sum += vector1.components[i] * vector2.components[i];
             }
-
-            public Vector GetDifference(Vector vector2)
-            {
-                for (int i = 0; i < this.getSize(); i++)
-                {
-                    this.components[i] -= vector2.components[i];
-                }
-                return this;
-            }
-
-            public Vector GetMultiplication(double scalar)
-            {
-                for (int i = 0; i < this.getSize(); i++)
-                {
-                    this.components[i] *= scalar;
-                }
-                return this;
-            }
-
-            public Vector Reverse()
-            {
-                for (int i = 0; i < this.getSize(); i++)
-                {
-                    this.components[i] *= -1;
-                }
-                return this;
-            }
-
-            public double GetComponent(int index)
-            {
-                return this.components[index];
-            }
-
-            public void SetComponent(int index, double newComponent)
-            {
-                this.components[index] = newComponent;
-            }
-
-            public static Vector GetSum(Vector vector1, Vector vector2)
-            {
-                //vector1.GetSum(vector2);
-                return vector1.GetSum(vector2);
-                //double[] array = new double[vector1.getSize()];
-
-                //for (int i = 0; i < vector1.getSize(); i++)
-                //{
-                //     array[i] = 
-                //}
-            }
-
-            public static Vector GetDifference(Vector vector1, Vector vector2)
-            {
-                return vector1.GetDifference(vector2);
-            }
-
-            public static double GetMultiple(Vector vector1, Vector vector2)
-            {
-                double sum = 0;
-                for (int i = 0; i < vector1.getSize(); i++)
-                {
-                    sum += vector1.components[i] * vector2.components[i]; 
-                }
-                return sum;
-            }
+            return sum;
+        }
 
         //}
         //static void Main(string[] args)
