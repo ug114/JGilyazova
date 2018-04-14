cbd75cc00c2974b538293a1de8464da01290eef7
user <user@WIN-2KR6UHLB0NV>
2018-04-14 12:13:45 +0700

    Changes in CSV.

--- Course2/CSV/Program.cs
+++ Course2/CSV/Program.cs
@@ -1,42 +1,126 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualBasic.FileIO;
-using System.Data;
 
 namespace CSV
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Print(string outputDocument, string inputDocument)
         {
-            using (StreamWriter writer = new StreamWriter("HTML.html"))
+            FileInfo outputFile = new FileInfo(outputDocument);
+            FileInfo inputFile = new FileInfo(inputDocument);
+            
+            if (!outputFile.Exists)
             {
-                using (StreamReader reader = new StreamReader("CSV.txt"))
+                throw new FileNotFoundException("CSV file not found.");
+            }
+             
+            if (!inputFile.Exists)
+            {
+                throw new FileNotFoundException("HTML file not found.");
+            }
+
+            using (StreamWriter writer = new StreamWriter(inputDocument))
+            {
+                using (StreamReader reader = new StreamReader(outputDocument))
                 {
-                    writer.WriteLine("<table>");
+                    writer.WriteLine("<!DOCTYPE HTML>");
+                    writer.WriteLine("<html>");
+                    writer.WriteLine("<head>");
+                    writer.WriteLine("<title>CSV to HTML</title>");
+                    writer.WriteLine("</head>");
+                    writer.WriteLine("<table border = '1'>");
                     string currentLine;
 
                     while ((currentLine = reader.ReadLine()) != null)
                     {
-                        writer.WriteLine("\t<tr>");
-                        string[] rowDetails;
-                        
-                        rowDetails = currentLine.Split(',');
+                        StringBuilder builder = new StringBuilder("<tr>\n<td>");
+                        int quotesCount = 0;
+                        int doubleQuotesCount = 0;
 
-                        foreach (string detail in rowDetails)
+                        for (int i = 0; i < currentLine.Length; i++)
                         {
-                            writer.WriteLine("\t\t<td>" + detail + "</td>");
+                            if (currentLine[i] == '"')
+                            {
+                                if (i < currentLine.Length - 1 && currentLine[i + 1] == '"')
+                                {
+                                    i++;
+                                    doubleQuotesCount++;
+                                    builder.Append('"');
+                                }
+                                else
+                                {
+                                    quotesCount++;
+                                }
+                            }
+
+                            if (currentLine[i] == '>')
+                            {
+                                builder.Append("&gt");
+                            }
+
+                            if (currentLine[i] == '<')
+                            {
+                                builder.Append("&lt");
+                            }
+
+                            if (currentLine[i] == '&')
+                            {
+                                builder.Append("&amp");
+                            }
+
+                            if (currentLine[i] == ',' && quotesCount % 2 == 1)
+                            {
+                                builder.Append(',');
+                            }
+
+                            if (currentLine[i] == ',' && quotesCount % 2 == 0)
+                            {
+                                builder.Append("</td>\n<td>");
+                            }
+
+                            if (currentLine[i] != ',' && currentLine[i] != '"' && currentLine[i] != '>' && currentLine[i] != '<' && currentLine[i] != '&')
+                            {
+                                builder.Append(currentLine[i]);
+                            }
+
+                            if (i == currentLine.Length - 1 && quotesCount % 2 == 0)
+                            {
+                                builder.Append("</td>");
+                            }
+                            else if (i == currentLine.Length - 1 && quotesCount % 2 == 1)
+                            {
+                                if ((currentLine = reader.ReadLine()) != null)
+                                {
+                                    builder.Append("<br>\n");
+                                    i = 0;
+                                    builder.Append(currentLine[i]);
+                                }
+                            }
                         }
 
-                        writer.WriteLine("\t</tr>");
+                        writer.WriteLine(builder.Append("\n</tr>").ToString());
                     }
 
                     writer.WriteLine("</table>");
+                    writer.WriteLine("</html>");
                 }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string inputDocument = "HTML.html";
+            string outputDocument = "CSV.txt";
+
+            try
+            {
+                Print(outputDocument, inputDocument);
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.ToString());
             }
         }
     }
